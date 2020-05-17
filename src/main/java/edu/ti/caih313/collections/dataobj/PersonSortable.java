9dc2f8c65b3a8f45a23901b01e243f1a84139fbd
Ngoldwag <55057184+Ngoldwag@users.noreply.github.com>
2020-05-17 17:17:41 -0500

    Add files via upload

---
+++ src/main/java/edu/ti/caih313/collections/dataobj/PersonSortable.java
@@ -0,0 +1,34 @@
+package edu.ti.caih313.collections.dataobj;
+
+public class PersonSortable extends Person implements Comparable<PersonSortable> {
+    public PersonSortable(Name name, Gender gender, int age) {
+        super(name, gender, age);
+    }
+
+    @Override
+    public int compareTo(PersonSortable otherPersonSortable) {
+        int compare;
+        if (getAge() == otherPersonSortable.getAge()) {
+            compare = 0;
+        } else if (getAge() > otherPersonSortable.getAge()) {
+            compare = -1;
+        } else {
+            compare = 1;
+        }
+        if (compare == 0) {
+            if (getGender() == otherPersonSortable.getGender()) {
+                compare = 0;
+            } else if (getGender().toString().equalsIgnoreCase("Male")) {
+                compare = 1;
+            } else {
+                compare = -1;
+            }
+        }
+        if (compare == 0) {
+            compare = getName().toString().compareTo(otherPersonSortable.getName().toString());
+        }
+        return compare;
+    }
+}
+
+

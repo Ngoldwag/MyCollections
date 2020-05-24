7b71e1f83cc89bf2f64a54244aeab91b8dfa4490
Chumi Goldwag <chumigoldwag@gmail.com>
2020-05-24 12:25:39 -0500

    Lab 11 complete

--- src/main/java/edu/ti/caih313/collections/aggregate/AgeAndGenderFilter.java
+++ src/main/java/edu/ti/caih313/collections/aggregate/AgeAndGenderFilter.java
@@ -1,10 +1,10 @@
 package edu.ti.caih313.collections.aggregate;
 
-import edu.ti.caih313.collections.dataobj.EmailAddress;
 import edu.ti.caih313.collections.dataobj.Name;
 import edu.ti.caih313.collections.dataobj.Person;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static edu.ti.caih313.collections.dataobj.EmailAddress.EmailType.*;
 import static edu.ti.caih313.collections.dataobj.EmailAddress.EmailType.HOME;
@@ -13,47 +13,47 @@ import static edu.ti.caih313.collections.dataobj.Person.Gender.*;
 public class AgeAndGenderFilter {
     public static void main(String... args) {
 
-                Person jamesBond = new Person(new Name("James", "Bond"), MALE, 23);
-                jamesBond.EmailAddress(SCHOOL, "james.bond@ti.htc.edu");
-                jamesBond.EmailAddress(WORK, "james.bond@work");
+        Person jamesFrank = new Person(new Name("James", "Frank"), MALE, 23);
+        jamesFrank.EmailAddress(SCHOOL, "james.frank@ti.htc.edu");
+        jamesFrank.EmailAddress(WORK, "james.frank@work");
 
-                Person jonesTruman= new Person(new Name("Jones", "Truman"),MALE , 33);
-                jonesTruman.EmailAddress(SCHOOL, "jones.truman@ti.htc.edu");
-                jonesTruman.EmailAddress(HOME, "jones.truman@work");
+        Person jonesSmith = new Person(new Name("Jones", "Smith"), MALE, 33);
+        jonesSmith.EmailAddress(SCHOOL, "jones.smith@ti.htc.edu");
+        jonesSmith.EmailAddress(HOME, "jones.smith@work");
 
-                Person donaldTrump = new Person(new Name("Donald", "Trump"), MALE, 13);
-                donaldTrump.EmailAddress(HOME, "donald.trump@theWhiteHouse");
-                donaldTrump.EmailAddress(WORK, "donald.trump@workWorkWork");
+        Person donaldTrump = new Person(new Name("Donald", "Trump"), MALE, 13);
+        donaldTrump.EmailAddress(HOME, "donald.trump@theWhiteHouse");
+        donaldTrump.EmailAddress(WORK, "donald.trump@workWorkWork");
 
-                Person michelleObama = new Person(new Name("Michelle", "Obama"), FEMALE,26);
-                michelleObama.EmailAddress(HOME, "michelle.obama@theWhiteHouse");
-                michelleObama.EmailAddress(WORK, "michelle.obama@workWorkWork");
-                michelleObama.EmailAddress(SCHOOL, "michelle.obama@ti.htc.edu");
+        Person michelleObama = new Person(new Name("Michelle", "Obama"), FEMALE, 26);
+        michelleObama.EmailAddress(HOME, "michelle.obama@theWhiteHouse");
+        michelleObama.EmailAddress(WORK, "michelle.obama@workWorkWork");
+        michelleObama.EmailAddress(SCHOOL, "michelle.obama@ti.htc.edu");
 
 
-                Person tracyCooman = new Person(new Name("Tracy", "Cooman"), FEMALE,56);
-                tracyCooman.EmailAddress(HOME, "tracy.Cooman@myHouse");
-                 tracyCooman.EmailAddress(WORK, "tracy.Cooman@workWorkWork");
-                tracyCooman.EmailAddress(SCHOOL, "tracy.Cooman@ti.htc.edu");
+        Person tracyCooman = new Person(new Name("Tracy", "Cooman"), FEMALE, 56);
+        tracyCooman.EmailAddress(HOME, "tracy.Cooman@myHouse");
+        tracyCooman.EmailAddress(WORK, "tracy.Cooman@workWorkWork");
+        tracyCooman.EmailAddress(SCHOOL, "tracy.Cooman@ti.htc.edu");
 
-                Person joeFrank = new Person(new Name("Joe", "Fraank"), MALE,26);
-                joeFrank.EmailAddress(HOME, "joeFrank@myHouse");
-                joeFrank.EmailAddress(WORK, "joeFrank@workWorkWork");
-                joeFrank.EmailAddress(SCHOOL, "joeFrank@ti.htc.edu");
+        Person joeFrank = new Person(new Name("Joe", "Frank"), MALE, 26);
+        joeFrank.EmailAddress(HOME, "joeFrank@myHouse");
+        joeFrank.EmailAddress(WORK, "joeFrank@workWorkWork");
+        joeFrank.EmailAddress(SCHOOL, "joeFrank@ti.htc.edu");
 
-                Person jillJacksWife = new Person(new Name("Jill", "JacksWife"), FEMALE,206);
-                jillJacksWife.EmailAddress(HOME, "jillJacksWife@myHouse");
-                jillJacksWife.EmailAddress(WORK, "jillJacksWife@workWorkWork");
-                jillJacksWife.EmailAddress(SCHOOL, "jillJacksWife@ti.htc.edu");
+        Person jillJacksWife = new Person(new Name("Jill", "JacksWife"), FEMALE, 206);
+        jillJacksWife.EmailAddress(HOME, "jillJacksWife@myHouse");
+        jillJacksWife.EmailAddress(WORK, "jillJacksWife@workWorkWork");
+        jillJacksWife.EmailAddress(SCHOOL, "jillJacksWife@ti.htc.edu");
 
-                Person GillyGillis = new Person(new Name("Gilly", "Gillis"), FEMALE,26);
-                GillyGillis.EmailAddress(HOME, "GillyGillis@myHouse");
-                GillyGillis.EmailAddress(WORK, "GillyGillis@workWorkWork");
-                GillyGillis.EmailAddress(SCHOOL, "GillyGillis@ti.htc.edu");
+        Person GillySmith = new Person(new Name("Gilly", "Smith"), FEMALE, 26);
+        GillySmith.EmailAddress(HOME, "GillyGillis@myHouse");
+        GillySmith.EmailAddress(WORK, "GillyGillis@workWorkWork");
+        GillySmith.EmailAddress(SCHOOL, "GillyGillis@ti.htc.edu");
 
-        Person personArray[] = {jamesBond,jonesTruman,donaldTrump,michelleObama,tracyCooman,joeFrank,jillJacksWife,GillyGillis};
+        Person personArray[] = {jamesFrank, jonesSmith, donaldTrump, michelleObama, tracyCooman, joeFrank, jillJacksWife, GillySmith};
 
-                System.out.print("\n");
+        System.out.print("\n");
         System.out.println("The youngest female");
         Arrays.stream(personArray)
                 .filter(p -> p.getGender() == FEMALE)
@@ -69,30 +69,30 @@ public class AgeAndGenderFilter {
 
         System.out.print("\n");
         System.out.println("All distinct last names of persons");
-        Arrays.stream(personArray)
+               Arrays.stream(personArray)
                 .map(p -> p.getName().getLastName())
                 .distinct()
                 .forEach(p -> System.out.println(p));
 
-        System.out.print("\n");
-        System.out.println("Histogram of last names");
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> frequency = new HashMap();
-        Arrays.stream(personArray)
-                .map(input -> input.getName().getLastName());
 
-//        while(input!=0) {
-//            histogram.put(input, 1);
-//            if (frequency.containsKey(input)) {
-//                increment(frequency, input);
-//                increment(histogram, input);
-//            } else {
-//                frequency.put(input, 1);
-//            }
-//        }
-//        .forEach(p -> System.out.println(frequency))
-//        public static<K> void increment(Map<K, Integer> map, K key) {
-//            map.put(key, map.get(key) + 1);
-//        }
+        System.out.print("\n");
+        System.out.println("Histogram of last names is ");
+        Stream<String> lastNames = Arrays.stream(personArray)
+                .map(p -> p.getName().getLastName());
+
+        ArrayList<String> lastNamesArray = new ArrayList<String>();
+
+        lastNames.forEach(p -> lastNamesArray.add(p));
+
+        HashMap<String, Integer> histogramFamMembers = new HashMap<>();
+
+        for(int i=0;i<lastNamesArray.size();i++) {
+            if (histogramFamMembers.containsKey(lastNamesArray.get(i))){
+                histogramFamMembers.put(lastNamesArray.get(i), histogramFamMembers.get(lastNamesArray.get(i))+1);
+            }else{
+                histogramFamMembers.put(lastNamesArray.get(i),1);
+            }
+        }
+        System.out.println(histogramFamMembers);
 
 }}
